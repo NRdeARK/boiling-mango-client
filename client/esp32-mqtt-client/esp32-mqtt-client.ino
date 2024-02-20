1140e9ff5dbe8d1a2b738a337be2cf75d9a090fb
Nataphan-Rak <nataphandeark@gmail.com>
2024-02-20 17:46:55 +0700

    Update esp32-mqtt-client.ino
    - update send mac address through body
    - update read data from response

--- client/esp32-mqtt-client/esp32-mqtt-client.ino
+++ client/esp32-mqtt-client/esp32-mqtt-client.ino
@@ -1,23 +1,35 @@
-#include <WiFi.h>
+#include <HTTPClient.h>
 #include <PubSubClient.h>
+#include <WiFi.h>
+
+// Time
+unsigned long lastTime;
 
 // WiFi
-const char *ssid = "Buddy"; // Enter your Wi-Fi name
-const char *password = "";  // Enter Wi-Fi password
+const char *ssid = "nodewifi";       // Enter your Wi-Fi name
+const char *password = "node123456"; // Enter Wi-Fi password
 
-// MQTT Broker
-const char *mqtt_broker = "192.168.249.184";
-const char *topic = "buddy/test/esp32/";
-const char *mqtt_username = "user1";
-const char *mqtt_password = "test";
-const int mqtt_port = 1883;
+// http server
+String http_hostname = "192.168.2.251";
+uint16_t http_port = 3000;
+
+const char *publishTopic[] = {
+    "buddy/publish/",
+};
+int publishNumber = 1;
+
+const char *subscribeTopic[] = {"buddy/command/1", "buddy/command/2",
+                                "buddy/command/3"};
+int subscribeNumber = 3;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
+
     // Set software serial baud to 115200;
     Serial.begin(115200);
+
     // Connecting to a WiFi network
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
@@ -25,26 +37,113 @@ void setup() {
         Serial.println("Connecting to WiFi..");
     }
     Serial.println("Connected to the Wi-Fi network");
-    
-    //connecting to a mqtt broker
+    HTTPClient http;
+    String payload;
+
+    // regiter esp32
+    String macAddress = WiFi.macAddress();
+    Serial.println(macAddress);
+    Serial.println("registering mac address");
+    http.begin(http_hostname, 3000, "/registerId");
+    payload = "{" + jsonFormat("macAddress", macAddress) + "}";
+    unsigned int lenghtOfPayload = (unsigned int) sizeof(payload);
+    char *packet;
+    String sPacket = payload;
+    sPacket.toCharArray(packet, sPacket.length()+1);
+    while (true) {
+        if (lastTime + 5000 < millis()) {
+            lastTime = millis();
+            http.addHeader("Content-Type", "application/json");
+            int httpCode = http.POST(payload);
+            if (httpCode > 0) {
+                // file found at server
+                if (httpCode == HTTP_CODE_OK) {
+                    payload = http.getString();
+                    Serial.println(payload);
+                    Serial.println("registered mac address");
+                    break;
+                } else {
+                    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+                }
+            } else {
+                Serial.println(httpCode);
+                Serial.printf("[HTTP] GET... failed, error: %s\n",
+                http.errorToString(httpCode).c_str());
+            }
+        }
+    }
+    http.end();
+
+    // request mqtt id
+    Serial.println("request mqtt identification");
+    http.begin(http_hostname, 3000, "/requestId");
+    while (true) {
+        if (lastTime + 5000 < millis()) {
+            lastTime = millis();
+            int httpCode =
+                http.POST("{}");
+            if (httpCode > 0) {
+                // file found at server
+                if (httpCode == HTTP_CODE_OK) {
+                    payload = http.getString();
+                    Serial.println(payload);
+                    Serial.println("recieve mqtt identification");
+                    break;
+                } else {
+                    Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+                }
+            } else {
+                Serial.println(httpCode);
+                Serial.printf("[HTTP] GET... failed, error: %s\n",
+                              http.errorToString(httpCode).c_str());
+            }
+        }
+    }
+    http.end();
+    // add 1 include end of character
+    Serial.println(jsonExtract(payload,"username"));
+    String hostname = jsonExtract(payload,"mqtt_hostname");
+    char mqtt_brokerArr[hostname.length()+1]; 
+    hostname.toCharArray(mqtt_brokerArr, hostname.length()+1);
+    char *mqtt_broker = mqtt_brokerArr;
+
+    String username = jsonExtract(payload,"username");
+    char mqtt_usernameArr[username.length()+1];
+    username.toCharArray(mqtt_usernameArr, username.length()+1);
+    char *mqtt_username = mqtt_usernameArr;
+
+    String password = jsonExtract(payload,"password");
+    char mqtt_passwordArr[password.length()+1];
+    password.toCharArray(mqtt_passwordArr, password.length()+1);
+    char *mqtt_password = mqtt_passwordArr;
+
+    int mqtt_port = jsonExtract(payload,"mqtt_port").toInt();
+
+    // connecting to a mqtt broker
+    Serial.println("connecting mqtt server");
     client.setServer(mqtt_broker, mqtt_port);
     client.setCallback(callback);
     while (!client.connected()) {
         String client_id = "esp32-client-";
         client_id += String(WiFi.macAddress());
-        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+        Serial.printf("The client %s connects to the public MQTT broker\n",
+                      client_id.c_str());
         if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
             Serial.println("Public EMQX MQTT broker connected");
         } else {
-            Serial.print("failed with state ");
-            Serial.print(client.state());
+            Serial.println("failed with state ");
+            Serial.println(client.state());
             delay(2000);
         }
     }
-    
+
     // Publish and subscribe
-    client.publish(topic, "Hi, I'm ESP32 ^^");
-    client.subscribe(topic);
+    for (int i = 0; i < publishNumber; i++) {
+        client.publish(publishTopic[i], "Hi, I'm ESP32 ^^");
+    }
+    for (int i = 0; i < subscribeNumber; i++) {
+        client.subscribe(subscribeTopic[i]);
+    }
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
@@ -52,12 +151,50 @@ void callback(char *topic, byte *payload, unsigned int length) {
     Serial.println(topic);
     Serial.print("Message:");
     for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
+        Serial.print((char)payload[i]);
     }
     Serial.println();
     Serial.println("-----------------------");
 }
 
-void loop() {
-    client.loop();
+String jsonFormat(String head, String data) {
+    return "\"" + head + "\" : \"" + data + "\"";
 }
+
+String jsonExtract(String json, String head) {
+    for (int i = 0; i < json.length(); i++) {
+        int count = 0;
+        for (int j = 0; j < head.length(); j++) {
+            if (i + j == json.length()){
+                return "-1";
+            }
+            if (json[i + j] == head[j]) {
+                count++;
+            } else {
+                count = 0;
+                break;
+            }
+        }
+        if (count != 0) {
+            String value = "";
+            bool start1 = 0;
+            bool start2 = 0;
+            for (int k = i; k < json.length(); k++) {
+                if (json[k] == ':') {
+                    start1 = 1;
+                } else if (start1 == 1 && json[k] == '\"') {
+                    if(start2==1){
+                        return value;
+                    }else{
+                        start2 = 1;
+                    }
+                } else if (start2 == 1) {
+                    value += json[k];
+                }
+            }
+        }
+    }
+    return "-1";
+}
+
+void loop() { client.loop(); }
