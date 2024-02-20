fc8c94c825e705332b9479e13703eea739f2bd0d
Nataphan-Rak <nataphandeark@gmail.com>
2024-02-20 14:17:23 +0700

    update http server

---
+++ test_http_server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/registerId", (req, res) => {
+    response = { 
+        message : "register success!",
+        macAddress : req.body.macAddress
+    };
+    console.log(response)
+    res.end(JSON.stringify(response));  
+});
+
+app.post("/requestId", (req, res) => {
+    response = {
+        message : "request success!",
+        mqtt_hostname : "192.168.4.184",
+        mqtt_port : "1883",
+        username : "test1",
+        password : "test"
+    };
+    console.log(response)
+    res.end(JSON.stringify(response));  
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
